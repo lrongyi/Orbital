25ef05e90fd6b84dda1f7d11082ab12f909fdfe4
muhdbhz <e1122341@u.nus.edu>
2024-06-19 13:14:24 +0800

    Small change: got rid of the gesture detector for title

--- lib/main.dart
+++ lib/main.dart
@@ -33,8 +33,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: LogIn(),
-      // home: SelectCategories(),
+      // home: LogIn(),
+      home: SelectCategories(),
       // home: Navigation(),
       // home: Billing()
     );
